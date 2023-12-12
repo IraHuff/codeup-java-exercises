6b6d04a467db286514c181d0311ab17efdfbb3db
Ira Huff <ira.m.huff@gmail.com>
2023-12-12 12:52:01 -0600

    added my favorite number

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello");
         System.out.println(", World!");
+
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+
+
     }
 }
